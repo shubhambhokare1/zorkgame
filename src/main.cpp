bf2fca77639f78cf1c8f94d03cc4016195d6267b
AKSHAY RAJ <akshayraj@Akshays-MacBook-Pro.local>
2018-10-17 12:18:06 -0400

    Added RapidXML

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,18 @@
 #include "item.h"
 #include "container.h"
 #include "creature.h"
+#include "rapidxml.hpp"
+#include "rapidxml_iterators.hpp"
+#include "rapidxml_print.hpp"
+#include "rapidxml_utils.hpp"
 #include <iostream>
+
 using namespace std;
+using namespace rapidxml;
 
 int main(int argc, char * argv[]) {
+    rapidxml::file<> xmlFile("input/*.xml"); // Default template is char
+    rapidxml::xml_document<> doc;
+    doc.parse<>(xmlFile.data());
 
 }
