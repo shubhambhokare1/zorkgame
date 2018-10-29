d9166f94665bf78f70726a4d37dcea6df5b2bc19
snebasu1 <basu13@purdue.edu>
2018-10-28 23:49:44 -0400

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 // Created by Shubham Bhokare on 10/15/18.
 //
 
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <vector>
 #include "room.h"
 #include "item.h"
 #include "container.h"
@@ -10,14 +16,24 @@
 #include "rapidxml_iterators.hpp"
 #include "rapidxml_print.hpp"
 #include "rapidxml_utils.hpp"
-#include <iostream>
+
 
 using namespace std;
 using namespace rapidxml;
 
 int main(int argc, char * argv[]) {
-    rapidxml::file<> xmlFile("input/*.xml"); // Default template is char
+	
+	if (argc != 1 ) { //error checking for innappropriate inputs or commands.  only xml file should be an input
+		cout << "Error! Enter: a.out filename.xml" << endl;
+		return 1;
+		}
+
+
+    rapidxml::file<> xmlFile(argv[1]); // Default template is char
     rapidxml::xml_document<> doc;
-    doc.parse<>(xmlFile.data());
+    doc.parse<>(xmlFile.data()); //default parse flag?
+
+	xml_node<> *node = doc.first_node(); //map node (root)
+
 
 }
